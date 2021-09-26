f9ab9e986c1eeee0f84c68ac33b9b8d73a5104d8
aldajo92 <alejodiscovery20@gmail.com>
2021-09-26 05:46:36 -0500

    temporal solution

--- app/src/main/java/com/projects/aldajo92/dialercombinations/MainActivity.kt
+++ app/src/main/java/com/projects/aldajo92/dialercombinations/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.projects.aldajo92.dialercombinations
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.projects.aldajo92.dialercombinations.databinding.ActivityMainBinding
@@ -11,6 +12,24 @@ class MainActivity : AppCompatActivity(), NumericDialListener {
 
     private lateinit var dialerView: NumericDialerView
 
+    // TODO: Temporal solution
+    private val mapValues = listOf(
+        "",
+        "",
+        "ABC",
+        "DEF",
+        "GHI",
+        "JKL",
+        "MNO",
+        "PRS",
+        "TUV",
+        "WXY"
+    ).mapIndexed { index, data ->
+        index to data
+    }.toMap()
+
+    private val combinationDial: CombinationDial = CombinationDialImpl(mapValues)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,10 +37,26 @@ class MainActivity : AppCompatActivity(), NumericDialListener {
         dialerView = binding.dialerView.apply {
             this.setNumericDialListener(this@MainActivity)
         }
+
+        binding.buttonCalculate.setOnClickListener {
+            val listIntegers = binding.textViewDialer.text.map {
+                it.toString().toInt()
+            }
+            val result = combinationDial.getAllCombinationList(listIntegers)
+            binding.editTextResult.setText(result.toString())
+            binding.editTextResult.visibility = View.VISIBLE
+        }
+
+        binding.buttonDelete.setOnClickListener {
+            binding.textViewDialer.text = ""
+            binding.editTextResult.setText("")
+        }
         setContentView(binding.root)
     }
 
     override fun onDialNumberListener(number: Int) {
+        val newText = binding.textViewDialer.text.toString() + number.toString()
+        binding.textViewDialer.text = newText
         Toast.makeText(this, number.toString(), Toast.LENGTH_SHORT).show()
     }
 }
